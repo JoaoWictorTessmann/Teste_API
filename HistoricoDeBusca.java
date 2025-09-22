99d4cc56ef4eeef860b297d32b2f46ae6447e176
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-09-22 20:52:26 -0300

    Foi adicionado o histórico desenvolvido

--- HistoricoDeBusca.java
+++ HistoricoDeBusca.java
@@ -1,21 +1,25 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class HistoricoDeBusca {
+
     private ArrayList<String> armazenarBuscas = new ArrayList<>();
 
-    public void adicionarCEP(String cep){
+    public void adicionarCEP(String cep) {
         armazenarBuscas.add("Foi pesquisado o CEP: " + cep);
     }
 
-    public void adicionarJog(String JOGADOR){
+    public void adicionarJog(String JOGADOR) {
         armazenarBuscas.add("Foi pesquisado o Jogador: " + JOGADOR);
     }
 
-    public void adicionarCNPJ(String CNPJ){
+    public void adicionarCNPJ(String CNPJ) {
         armazenarBuscas.add("Foi pesquisado o CNPJ: " + CNPJ);
     }
 
-    public ArrayList<String> getHistorico(){
-        return armazenarBuscas;
+    public List<String> getHistorico() {
+        int tamanho = armazenarBuscas.size();
+        int inicio = Math.max(0, tamanho - 5);
+        return armazenarBuscas.subList(inicio, tamanho);
     }
 }
